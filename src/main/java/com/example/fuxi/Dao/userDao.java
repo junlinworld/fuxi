a0d0d149bda02435f043679d769ff09e0af62dad
junlinworld <33860483+junlinworld@users.noreply.github.com>
2019-02-21 17:01:02 +0800

    Initial commit

---
+++ src/main/java/com/example/fuxi/Dao/userDao.java
@@ -0,0 +1,15 @@
+package com.example.fuxi.Dao;
+
+import com.example.fuxi.pojo.User;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface userDao {
+    int insertUser(@Param("user") User user);
+    int deleteUser(@Param("userName") String userName);
+    int updateUser(@Param("user") User user);
+    User getUserByName(@Param("userName") String userName);
+
+}
+
---
+++ src/main/java/com/example/fuxi/pojo/User.java
@@ -0,0 +1,34 @@
+package com.example.fuxi.pojo;
+
+import org.apache.ibatis.type.Alias;
+
+@Alias("user")
+public class User {
+    int userId;
+    String userName;
+    String userPassword;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+}
