c483a6fcb71df03a5586a7f70c470c1e5756b8c3
junlinworld <33860483+junlinworld@users.noreply.github.com>
2019-02-21 17:58:10 +0800

    搭建Java项目复习
    已完成mapper，pojo，dao的搭建，还差service，controller

--- src/main/java/com/example/fuxi/FuxiApplication.java
+++ src/main/java/com/example/fuxi/FuxiApplication.java
@@ -1,9 +1,11 @@
 package com.example.fuxi;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.fuxi.Dao")
 public class FuxiApplication {
 
     public static void main(String[] args) {
